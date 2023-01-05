7af83a18989618ec06714c8a8d840a020c6e3bbf
LouisCoilliot <louis.coilliot@takeda.com>
2023-01-05 15:21:06 +0100

    umask, again; lint

--- manifests/umask.pp
+++ manifests/umask.pp
@@ -9,14 +9,22 @@ class hardening::umask {
     match    => '^\s*umask\s+\d'
   }
 
+  # Ensure the Default Umask is Set Correctly in /etc/profile
+  # xccdf_org.ssgproject.content_rule_accounts_umask_etc_profile
+  file_line { 'umask - profile':
+    path     => '/etc/profile',
+    line     => '    umask 027',
+    multiple => true,
+    match    => '^\s*umask\s+\d'
+  }
+
   # Ensure the Default Umask is Set Correctly in login.defs
   # xccdf_org.ssgproject.content_rule_accounts_umask_etc_login_defs
   file_line { 'umask - login.defs':
-    path     => '/etc/logins.def',
+    path     => '/etc/login.defs',
     line     => 'UMASK       027',
     multiple => false,
     match    => '^\s*UMASK\s+\d'
   }
-
 
 }
