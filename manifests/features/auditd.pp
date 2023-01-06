4d031962df5a7fb8beb3162d47d807870935818e
LouisCoilliot <louis.coilliot@takeda.com>
2023-01-06 18:59:38 +0100

    Auditd

--- manifests/features/auditd.pp
+++ manifests/features/auditd.pp
@@ -7,6 +7,7 @@ class hardening::features::auditd {
       owner  => 'root',
       group  => 'root',
       mode   => '0640',
+      notify => Exec['refresh rules'],
   }
 
   file {
@@ -20,6 +21,42 @@ class hardening::features::auditd {
     'access.rules' :
       path   => "${rulespath}/access.rules",
       source => 'puppet:///modules/hardening/auditd/access.rules',;
+    'modules.rules' :
+      path   => "${rulespath}/modules.rules",
+      source => 'puppet:///modules/hardening/auditd/modules.rules',;
+    'logins.rules' :
+      path   => "${rulespath}/logins.rules",
+      source => 'puppet:///modules/hardening/auditd/logins.rules',;
+    'time-change.rules' :
+      path   => "${rulespath}/time-change.rules",
+      source => 'puppet:///modules/hardening/auditd/time-change.rules',;
+    'audit_time_rules.rules' :
+      path   => "${rulespath}/audit_time_rules.rules",
+      source => 'puppet:///modules/hardening/auditd/audit_time_rules.rules',;
+    'MAC-policy.rules' :
+      path   => "${rulespath}/MAC-policy.rules",
+      source => 'puppet:///modules/hardening/auditd/MAC-policy.rules',;
+    'audit_rules_networkconfig_modification.rules' :
+      path   => "${rulespath}/audit_rules_networkconfig_modification.rules",
+      source => 'puppet:///modules/hardening/auditd/audit_rules_networkconfig_modification.rules',;
+    'actions.rules' :
+      path   => "${rulespath}/actions.rules",
+      source => 'puppet:///modules/hardening/auditd/actions.rules',;
+    'session.rules' :
+      path   => "${rulespath}/session.rules",
+      source => 'puppet:///modules/hardening/auditd/session.rules',;
+    'audit_rules_usergroup_modification.rules' :
+      path   => "${rulespath}/audit_rules_usergroup_modification.rules",
+      source => 'puppet:///modules/hardening/auditd/audit_rules_usergroup_modification.rules',;
+    'immutable.rules' :
+      path   => "${rulespath}/immutable.rules",
+      source => 'puppet:///modules/hardening/auditd/immutable.rules',;
+  }
+
+  exec { 'refresh rules':
+    path        => ['/sbin', '/usr/sbin'],
+    command     => 'augenrules',
+    refreshonly => 'true',
   }
 
 }
